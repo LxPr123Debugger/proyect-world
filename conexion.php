df63cf8b95945602339b5119b7b145cbeb027a62
LxPr123Debugger <leopenuelaleon@gmail.com>
2026-04-29 02:54:03 +0000

    aaaaaaaaaaaa

--- conexion.php
+++ conexion.php
@@ -1,14 +1,9 @@
 <?php
-// Intentamos conectar sin contraseña (común en entornos de desarrollo)
-$conexion = new mysqli("localhost", "root", "", "world");
-
-// Si falla, intentamos con el usuario por defecto de Codespaces
-if ($conexion->connect_error) {
-    $conexion = new mysqli("127.0.0.1", "root", "root", "world");
-}
+// Conexión estándar sin contraseña
+$conexion = new mysqli("127.0.0.1", "root", "", "world");
 
 if ($conexion->connect_error) {
-    die("Error final de conexión: " . $conexion->connect_error);
+    die("Error de conexión: " . $conexion->connect_error);
 }
 
 $conexion->set_charset("utf8mb4");
