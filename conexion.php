ca5acb37216fd9c222ae4bcc7ce9c7d93a156d9a
LxPr123Debugger <leopenuelaleon@gmail.com>
2026-04-29 02:57:15 +0000

    bbbbbbbbb

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php
-// Conexión estándar sin contraseña
-$conexion = new mysqli("127.0.0.1", "root", "", "world");
+// Usamos el nuevo usuario 'admin' y la contraseña 'admin123'
+$conexion = new mysqli("127.0.0.1", "admin", "admin123", "world");
 
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
